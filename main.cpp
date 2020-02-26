bbb0e4f21a877d09387d34c1964e2f2d724abb20
osama200949 <61515361+osama200949@users.noreply.github.com>
2020-02-27 02:02:52 +0800

    Add files via upload

---
+++ main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+#include <graphics.h>
+#include "GameH/Game.hpp"
+
+using namespace std;
+
+int main(){
+    
+    Game fastCoder;
+    fastCoder.start();
+
+    system("pause");
+}
